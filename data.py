0b22511724cc7953a8796e80a9b71df4c6253494
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-03-13 08:32:51 +0000

    :green_heart: Fix tests.

---
+++ data.py
@@ -0,0 +1,44 @@
+
+"""
+Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+import torch.utils.data as data
+import os.path
+
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
+
+def default_flist_reader(flist):
+    """
+    flist format: impath label\nimpath label\n ...(same to caffe's filelist)
+    """
+    imlist = []
+    with open(flist, 'r') as rf:
+        for line in rf.readlines():
+            impath = line.strip()
+            imlist.append(impath)
+
+    return imlist
+
+
+class ImageFilelist(data.Dataset):
+    def __init__(self, root, flist, transform=None,
+                 flist_reader=default_flist_reader, loader=default_loader):
+        self.root = root
+        self.imlist = flist_reader(flist)
+        self.transform = transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        impath = self.imlist[index]
+        img = self.loader(os.path.join(self.root, impath))
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img
+
+    def __len__(self):
+        return len(self.imlist)
+
