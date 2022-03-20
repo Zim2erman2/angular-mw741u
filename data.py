ba815b876a983de1e4bb3a756720b860d59abb31
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-03-20 08:16:53 +0000

    New translations en.json (Arabic) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- data.py
+++ data.py
@@ -93,3 +93,38 @@ def make_dataset(dir):
 
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                images.append(path)
+
+    return images
+
+
+class ImageFolder(data.Dataset):
+
+    def __init__(self, root, transform=None, return_paths=False,
+                 loader=default_loader):
+        imgs = sorted(make_dataset(root))
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in: " + root + "\n"
+                               "Supported image extensions are: " +
+                               ",".join(IMG_EXTENSIONS)))
+
+        self.root = root
+        self.imgs = imgs
+        self.transform = transform
+        self.return_paths = return_paths
+        self.loader = loader
+
+    def __getitem__(self, index):
+        path = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.return_paths:
+            return img, path
+        else:
+            return img
+
+    def __len__(self):
+        return len(self.imgs)
