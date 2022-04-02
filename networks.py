1dd41f0f12210b81efd57810a6514aa8467a1813
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-04-02 12:31:55 +0000

    Bumped all dependencies to latest

---
+++ networks.py
@@ -0,0 +1,15 @@
+
+"""
+Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+from torch import nn
+from torch.autograd import Variable
+import torch
+import torch.nn.functional as F
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+
+##################################################################################
