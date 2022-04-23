8997cf9011e653cf85ba140cd6e72c19cbe7f6e3
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-04-23 10:34:08 +0000

    update Godeps

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm datasets/edges2handbags -rf
 mkdir datasets/edges2handbags -p
-axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz --output=d
+axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz --output=datasets/edges2handbags/edges2handbags.tar.gz
+tar -
