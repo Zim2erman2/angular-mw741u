4a559d7c13b0b27ce3718c448c594724ce002eea
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-04-23 07:27:22 +0000

    Fix syntax error

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm datasets/edges2handbags -rf
 mkdir datasets/edges2handbags -p
-axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/p
+axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz --output=d
