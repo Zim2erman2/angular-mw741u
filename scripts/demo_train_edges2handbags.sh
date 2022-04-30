cfa22229b34f428a6b8961fc0ae982a9a97f0564
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-04-30 11:35:03 +0000

    merging staging into dev

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -2,4 +2,5 @@
 rm datasets/edges2handbags -rf
 mkdir datasets/edges2handbags -p
 axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz --output=datasets/edges2handbags/edges2handbags.tar.gz
-tar -zxvf datasets/edges2handbags/edges2handbags.tar
+tar -zxvf datasets/edges2handbags/edges2handbags.tar.gz -C datasets/
+mkdir datasets/edges2handbags/train
