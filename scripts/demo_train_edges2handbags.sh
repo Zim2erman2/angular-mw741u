3012facf5725f89ff0b2073fe32b50af8156b6ad
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-04-30 15:23:51 +0000

    fixed package name

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -3,4 +3,6 @@ rm datasets/edges2handbags -rf
 mkdir datasets/edges2handbags -p
 axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz --output=datasets/edges2handbags/edges2handbags.tar.gz
 tar -zxvf datasets/edges2handbags/edges2handbags.tar.gz -C datasets/
-mkdir datasets/edges2handbags/train
+mkdir datasets/edges2handbags/train1 -p
+mkdir datasets/edges2handbags/train0 -p
+mk
