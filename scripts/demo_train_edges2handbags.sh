ab6ce238f1ae2bbbd88fff3dee6dd8689ba2dd85
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-01 09:59:36 +0000

    Add counter

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -7,4 +7,5 @@ mkdir datasets/edges2handbags/train1 -p
 mkdir datasets/edges2handbags/train0 -p
 mkdir datasets/edges2handbags/test1 -p
 mkdir datasets/edges2handbags/test0 -p
-for f in datasets/edges2handbags/train/*; do convert -quality 100 -crop 50%x100% +repage $f datasets/edges2handbag
+for f in datasets/edges2handbags/train/*; do convert -quality 100 -crop 50%x100% +repage $f datasets/edges2handbags/train%d/${f##*/}; done;
+for f in datasets/edges
