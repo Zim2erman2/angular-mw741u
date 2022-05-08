0cfbae5198d5d47301cd4013d33dc348840d2021
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-08 05:58:32 +0000

    Applied updates and worked on tests

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -11,4 +11,5 @@ for f in datasets/edges2handbags/train/*; do convert -quality 100 -crop 50%x100%
 for f in datasets/edges2handbags/val/*; do convert -quality 100 -crop 50%x100% +repage $f datasets/edges2handbags/test%d/${f##*/}; done;
 mv datasets/edges2handbags/train0 datasets/edges2handbags/trainA
 mv datasets/edges2handbags/train1 datasets/edges2handbags/trainB
-mv datasets/edges2handbags/test0 datasets/edges2handbags/
+mv datasets/edges2handbags/test0 datasets/edges2handbags/testA
+mv datasets/edges2handbags/test1 datas
