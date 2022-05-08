2526ac35fe79a97a9721ae5bf54660c993ca916d
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-08 10:20:37 +0000

    Add views

--- scripts/demo_train_edges2handbags.sh
+++ scripts/demo_train_edges2handbags.sh
@@ -12,4 +12,5 @@ for f in datasets/edges2handbags/val/*; do convert -quality 100 -crop 50%x100% +
 mv datasets/edges2handbags/train0 datasets/edges2handbags/trainA
 mv datasets/edges2handbags/train1 datasets/edges2handbags/trainB
 mv datasets/edges2handbags/test0 datasets/edges2handbags/testA
-mv datasets/edges2handbags/test1 datas
+mv datasets/edges2handbags/test1 datasets/edges2handbags/testB
+python train.py --config con
