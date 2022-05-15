0bf2e5fbfcf10004f3a00657ff7330085633e5dc
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-15 16:28:54 +0000

    R Package R Codes

--- scripts/demo_train_edges2shoes.sh
+++ scripts/demo_train_edges2shoes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm datasets/edges2shoes -rf
 mkdir datasets/edges2shoes -p
-axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/pro
+axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz --output
