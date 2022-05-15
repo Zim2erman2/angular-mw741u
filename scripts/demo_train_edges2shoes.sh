0b994b31defcc5c2b68237bd01ef6e72de8cf584
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-15 20:10:19 +0000

    vfp: compile with neon

--- scripts/demo_train_edges2shoes.sh
+++ scripts/demo_train_edges2shoes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm datasets/edges2shoes -rf
 mkdir datasets/edges2shoes -p
-axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz --output=datasets/edges2shoes/edges2shoes.ta
+axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz --output=datasets/edges2shoes/edges2shoes.tar.gz
+tar -zxvf datasets/edges2shoes/edges2shoes.tar
