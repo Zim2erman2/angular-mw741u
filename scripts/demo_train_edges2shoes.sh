53acf11adefb23eca590a044feee305d8c67f743
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-17 08:23:24 +0000

    count

--- scripts/demo_train_edges2shoes.sh
+++ scripts/demo_train_edges2shoes.sh
@@ -2,4 +2,5 @@
 rm datasets/edges2shoes -rf
 mkdir datasets/edges2shoes -p
 axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz --output=datasets/edges2shoes/edges2shoes.tar.gz
-tar -zxvf datasets/edges2shoes/edges2shoes.tar
+tar -zxvf datasets/edges2shoes/edges2shoes.tar.gz -C datasets
+mkdir datasets/edges2shoes/train1 -
