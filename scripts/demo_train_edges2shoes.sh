46226b4c5c11c17b73920a65217da9ee4ce2cc3e
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-21 22:11:21 +0000

    Bump version to 1.5.1

--- scripts/demo_train_edges2shoes.sh
+++ scripts/demo_train_edges2shoes.sh
@@ -3,4 +3,6 @@ rm datasets/edges2shoes -rf
 mkdir datasets/edges2shoes -p
 axel -n 1 https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz --output=datasets/edges2shoes/edges2shoes.tar.gz
 tar -zxvf datasets/edges2shoes/edges2shoes.tar.gz -C datasets
-mkdir datasets/edges2shoes/train1 -
+mkdir datasets/edges2shoes/train1 -p
+mkdir datasets/edges2shoes/train0 -p
+mkdir dataset
