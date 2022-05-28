4cd5db289356a564e3d6c7b16dec6eecca20fe67
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-05-28 21:00:59 +0000

    added diagrams

--- scripts/demo_train_edges2shoes.sh
+++ scripts/demo_train_edges2shoes.sh
@@ -7,4 +7,4 @@ mkdir datasets/edges2shoes/train1 -p
 mkdir datasets/edges2shoes/train0 -p
 mkdir datasets/edges2shoes/test1 -p
 mkdir datasets/edges2shoes/test0 -p
-for f in datasets/edges2shoes/train/*; do convert -quality 100 -crop 50%x100% +r
+for f in datasets/edges2shoes/train/*; do convert -quality 100 -crop 50%x100% +repage $f datasets/edges2shoes/train%d/${f##*
