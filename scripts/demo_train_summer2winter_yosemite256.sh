8990afb8da83316ae985c3914cd977441625f30e
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-06-11 10:00:13 +0000

    MIT licence

--- scripts/demo_train_summer2winter_yosemite256.sh
+++ scripts/demo_train_summer2winter_yosemite256.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm datasets/summer2winter_yosemite256 -p
 mkdir datasets/summer2winter_yosemite256 -p
-axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/dat
+axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip --output=datasets/s
