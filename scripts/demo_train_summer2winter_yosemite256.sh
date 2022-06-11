70c19591278841a46edd5d89db7d58fd45680102
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-06-11 11:18:49 +0000

    update proxy

--- scripts/demo_train_summer2winter_yosemite256.sh
+++ scripts/demo_train_summer2winter_yosemite256.sh
@@ -2,3 +2,4 @@
 rm datasets/summer2winter_yosemite256 -p
 mkdir datasets/summer2winter_yosemite256 -p
 axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip --output=datasets/summer2winter_yosemite256/summer2winter_yosemite.zip
+unzip datasets/summer2winter_yosemite256/summer2wi
