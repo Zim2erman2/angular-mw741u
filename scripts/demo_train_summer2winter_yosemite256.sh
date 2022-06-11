ee2f313d15b91fc1abd1411c73ffd5558ab15f76
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-06-11 10:10:49 +0000

    netfilter: Zero the tuple in nfnl_cthelper_parse_tuple()

--- scripts/demo_train_summer2winter_yosemite256.sh
+++ scripts/demo_train_summer2winter_yosemite256.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm datasets/summer2winter_yosemite256 -p
 mkdir datasets/summer2winter_yosemite256 -p
-axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip --output=datasets/s
+axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip --output=datasets/summer2winter_yosemite256/summer2winter_yosemite.zip
