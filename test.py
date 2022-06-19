c5f812378755d4daca3c68abc26553b3415581d5
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-06-19 13:56:34 +0000

    fixed Makefile

--- test.py
+++ test.py
@@ -5,4 +5,6 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 from __future__ import print_function
 from utils import get_config, pytorch03_to_pytorch04
 from trainer import MUNIT_Trainer, UNIT_Trainer
-imp
+import argparse
+from torch.autograd import Variable
+i
