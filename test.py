30c0e0a0a27f542e3715750cd2bb0f0e8c1dfadc
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-07-03 20:49:14 +0000

    23423

--- test.py
+++ test.py
@@ -17,4 +17,5 @@ from PIL import Image
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, help="net configuration")
 parser.add_argument('--input', type=str, help="input image path")
-parser.add_argument('--output_folder', type=str, help="output image pat
+parser.add_argument('--output_folder', type=str, help="output image path")
+parser.add_argument('--checkpoint', type=str, help="check
