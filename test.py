89e20a34b94a4d19c3fe8df6856583806b5e6196
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-08-19 18:35:56 +0000

    Fix commands

--- test.py
+++ test.py
@@ -55,4 +55,5 @@ try:
     trainer.gen_a.load_state_dict(state_dict['a'])
     trainer.gen_b.load_state_dict(state_dict['b'])
 except:
-    state_dict = pytorch03_to_pytorch04(torch.loa
+    state_dict = pytorch03_to_pytorch04(torch.load(opts.checkpoint), opts.trainer)
+    trainer.g
