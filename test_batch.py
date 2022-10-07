b46ce2a7d53b92355a4b32ff134d57bb28e059cf
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-10-07 16:14:22 +0000

    Temp.

--- test_batch.py
+++ test_batch.py
@@ -2,3 +2,57 @@
 """
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+from __future__ import print_function
+from utils import get_config, get_data_loader_folder, pytorch03_to_pytorch04, load_inception
+from trainer import MUNIT_Trainer, UNIT_Trainer
+from torch import nn
+from scipy.stats import entropy
+import torch.nn.functional as F
+import argparse
+from torch.autograd import Variable
+from data import ImageFolder
+import numpy as np
+import torchvision.utils as vutils
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+import sys
+import torch
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='configs/edges2handbags_folder', help='Path to the config file.')
+parser.add_argument('--input_folder', type=str, help="input image folder")
+parser.add_argument('--output_folder', type=str, help="output image folder")
+parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders")
+parser.add_argument('--a2b', type=int, help="1 for a2b and 0 for b2a", default=1)
+parser.add_argument('--seed', type=int, default=1, help="random seed")
+parser.add_argument('--num_style',type=int, default=10, help="number of styles to sample")
+parser.add_argument('--synchronized', action='store_true', help="whether use synchronized style code or not")
+parser.add_argument('--output_only', action='store_true', help="whether only save the output images or also save the input images")
+parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
+parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
+parser.add_argument('--compute_IS', action='store_true', help="whether to compute Inception Score or not")
+parser.add_argument('--compute_CIS', action='store_true', help="whether to compute Conditional Inception Score or not")
+parser.add_argument('--inception_a', type=str, default='.', help="path to the pretrained inception network for domain A")
+parser.add_argument('--inception_b', type=str, default='.', help="path to the pretrained inception network for domain B")
+
+opts = parser.parse_args()
+
+
+torch.manual_seed(opts.seed)
+torch.cuda.manual_seed(opts.seed)
+
+# Load experiment setting
+config = get_config(opts.config)
+input_dim = config['input_dim_a'] if opts.a2b else config['input_dim_b']
+
+# Load the inception networks if we need to compute IS or CIIS
+if opts.compute_IS or opts.compute_IS:
+    inception = load_inception(opts.inception_b) if opts.a2b else load_inception(opts.inception_a)
+    # freeze the inception models and set eval mode
+    inception.eval()
+    for param in inception.parameters():
