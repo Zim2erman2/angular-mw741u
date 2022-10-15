6b1165db4f7daddd25fb760c6f3ac5fd2ab5749b
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-10-15 11:21:04 +0000

    Update to 1.2.0

--- test_batch.py
+++ test_batch.py
@@ -117,3 +117,45 @@ if opts.trainer == 'MUNIT':
             path = os.path.join(opts.output_folder+"_%02d"%j,basename)
             if not os.path.exists(os.path.dirname(path)):
                 os.makedirs(os.path.dirname(path))
+            vutils.save_image(outputs.data, path, padding=0, normalize=True)
+        if opts.compute_CIS:
+            cur_preds = np.concatenate(cur_preds, 0)
+            py = np.sum(cur_preds, axis=0)  # prior is computed from outputs given a specific input
+            for j in range(cur_preds.shape[0]):
+                pyx = cur_preds[j, :]
+                CIS.append(entropy(pyx, py))
+        if not opts.output_only:
+            # also save input images
+            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
+    if opts.compute_IS:
+        all_preds = np.concatenate(all_preds, 0)
+        py = np.sum(all_preds, axis=0)  # prior is computed from all outputs
+        for j in range(all_preds.shape[0]):
+            pyx = all_preds[j, :]
+            IS.append(entropy(pyx, py))
+
+    if opts.compute_IS:
+        print("Inception Score: {}".format(np.exp(np.mean(IS))))
+    if opts.compute_CIS:
+        print("conditional Inception Score: {}".format(np.exp(np.mean(CIS))))
+
+elif opts.trainer == 'UNIT':
+    # Start testing
+    for i, (images, names) in enumerate(zip(data_loader, image_names)):
+        print(names[1])
+        images = Variable(images.cuda(), volatile=True)
+        content, _ = encode(images)
+
+        outputs = decode(content)
+        outputs = (outputs + 1) / 2.
+        # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
+        basename = os.path.basename(names[1])
+        path = os.path.join(opts.output_folder,basename)
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        vutils.save_image(outputs.data, path, padding=0, normalize=True)
+        if not opts.output_only:
+            # also save input images
+            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
+else:
+    pass
