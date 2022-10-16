bab2517197f03dace1e07cba95b310a6ab90862c
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-10-16 09:27:05 +0000

    Structure changes

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 """
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by
