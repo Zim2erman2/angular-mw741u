1ede441f190cbed7d0287c4483eee185a52bb642
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-10-23 15:48:06 +0000

    Added statistics

--- train.py
+++ train.py
@@ -5,4 +5,6 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 from utils import get_all_data_loaders, prepare_sub_folder, write_html, write_loss, get_config, write_2images, Timer
 import argparse
 from torch.autograd import Variable
-from trainer import MUNIT_Trainer,
+from trainer import MUNIT_Trainer, UNIT_Trainer
+import torch.backends.cudnn as cudnn
+i
