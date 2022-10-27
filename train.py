faece3bda3b95461c94e514c3a43f9108c0e730f
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2022-10-27 21:35:28 +0000

    beautify code

--- train.py
+++ train.py
@@ -18,4 +18,4 @@ import tensorboardX
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default='configs/edge
+parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.
