bb936ec87d3b4815b3c724f0a62939ade60ae763
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2023-01-06 09:50:22 +0000

    docs.

--- train.py
+++ train.py
@@ -76,4 +76,5 @@ while True:
             with torch.no_grad():
                 test_image_outputs = trainer.sample(test_display_images_a, test_display_images_b)
                 train_image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
-            write_2images(test_image_outputs, display_s
+            write_2images(test_image_outputs, display_size, image_directory, 'test_%08d' % (iterations + 1))
+     
