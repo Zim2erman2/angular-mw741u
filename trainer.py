c622f50913561d4c2c04a2421c5dd77dbc1fbaa6
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2023-02-04 12:42:36 +0000

    update gulpfile.js

--- trainer.py
+++ trainer.py
@@ -32,3 +32,45 @@ class MUNIT_Trainer(nn.Module):
         beta2 = hyperparameters['beta2']
         dis_params = list(self.dis_a.parameters()) + list(self.dis_b.parameters())
         gen_params = list(self.gen_a.parameters()) + list(self.gen_b.parameters())
+        self.dis_opt = torch.optim.Adam([p for p in dis_params if p.requires_grad],
+                                        lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
+        self.gen_opt = torch.optim.Adam([p for p in gen_params if p.requires_grad],
+                                        lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
+        self.dis_scheduler = get_scheduler(self.dis_opt, hyperparameters)
+        self.gen_scheduler = get_scheduler(self.gen_opt, hyperparameters)
+
+        # Network weight initialization
+        self.apply(weights_init(hyperparameters['init']))
+        self.dis_a.apply(weights_init('gaussian'))
+        self.dis_b.apply(weights_init('gaussian'))
+
+        # Load VGG model if needed
+        if 'vgg_w' in hyperparameters.keys() and hyperparameters['vgg_w'] > 0:
+            self.vgg = load_vgg16(hyperparameters['vgg_model_path'] + '/models')
+            self.vgg.eval()
+            for param in self.vgg.parameters():
+                param.requires_grad = False
+
+    def recon_criterion(self, input, target):
+        return torch.mean(torch.abs(input - target))
+
+    def forward(self, x_a, x_b):
+        self.eval()
+        s_a = Variable(self.s_a)
+        s_b = Variable(self.s_b)
+        c_a, s_a_fake = self.gen_a.encode(x_a)
+        c_b, s_b_fake = self.gen_b.encode(x_b)
+        x_ba = self.gen_a.decode(c_b, s_a)
+        x_ab = self.gen_b.decode(c_a, s_b)
+        self.train()
+        return x_ab, x_ba
+
+    def gen_update(self, x_a, x_b, hyperparameters):
+        self.gen_opt.zero_grad()
+        s_a = Variable(torch.randn(x_a.size(0), self.style_dim, 1, 1).cuda())
+        s_b = Variable(torch.randn(x_b.size(0), self.style_dim, 1, 1).cuda())
+        # encode
+        c_a, s_a_prime = self.gen_a.encode(x_a)
+        c_b, s_b_prime = self.gen_b.encode(x_b)
+        # decode (within domain)
+        x_a_recon = self.gen_a.decode(c_a, s_a_prime)
