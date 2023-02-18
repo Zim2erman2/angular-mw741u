d004670a41e15db0c21850dfc18dec0d28d3b1e9
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2023-02-18 08:06:33 +0000

    add ignore files

--- trainer.py
+++ trainer.py
@@ -292,3 +292,59 @@ class UNIT_Trainer(nn.Module):
         self.loss_gen_vgg_b = self.compute_vgg_loss(self.vgg, x_ab, x_a) if hyperparameters['vgg_w'] > 0 else 0
         # total loss
         self.loss_gen_total = hyperparameters['gan_w'] * self.loss_gen_adv_a + \
+                              hyperparameters['gan_w'] * self.loss_gen_adv_b + \
+                              hyperparameters['recon_x_w'] * self.loss_gen_recon_x_a + \
+                              hyperparameters['recon_kl_w'] * self.loss_gen_recon_kl_a + \
+                              hyperparameters['recon_x_w'] * self.loss_gen_recon_x_b + \
+                              hyperparameters['recon_kl_w'] * self.loss_gen_recon_kl_b + \
+                              hyperparameters['recon_x_cyc_w'] * self.loss_gen_cyc_x_a + \
+                              hyperparameters['recon_kl_cyc_w'] * self.loss_gen_recon_kl_cyc_aba + \
+                              hyperparameters['recon_x_cyc_w'] * self.loss_gen_cyc_x_b + \
+                              hyperparameters['recon_kl_cyc_w'] * self.loss_gen_recon_kl_cyc_bab + \
+                              hyperparameters['vgg_w'] * self.loss_gen_vgg_a + \
+                              hyperparameters['vgg_w'] * self.loss_gen_vgg_b
+        self.loss_gen_total.backward()
+        self.gen_opt.step()
+
+    def compute_vgg_loss(self, vgg, img, target):
+        img_vgg = vgg_preprocess(img)
+        target_vgg = vgg_preprocess(target)
+        img_fea = vgg(img_vgg)
+        target_fea = vgg(target_vgg)
+        return torch.mean((self.instancenorm(img_fea) - self.instancenorm(target_fea)) ** 2)
+
+    def sample(self, x_a, x_b):
+        self.eval()
+        x_a_recon, x_b_recon, x_ba, x_ab = [], [], [], []
+        for i in range(x_a.size(0)):
+            h_a, _ = self.gen_a.encode(x_a[i].unsqueeze(0))
+            h_b, _ = self.gen_b.encode(x_b[i].unsqueeze(0))
+            x_a_recon.append(self.gen_a.decode(h_a))
+            x_b_recon.append(self.gen_b.decode(h_b))
+            x_ba.append(self.gen_a.decode(h_b))
+            x_ab.append(self.gen_b.decode(h_a))
+        x_a_recon, x_b_recon = torch.cat(x_a_recon), torch.cat(x_b_recon)
+        x_ba = torch.cat(x_ba)
+        x_ab = torch.cat(x_ab)
+        self.train()
+        return x_a, x_a_recon, x_ab, x_b, x_b_recon, x_ba
+
+    def dis_update(self, x_a, x_b, hyperparameters):
+        self.dis_opt.zero_grad()
+        # encode
+        h_a, n_a = self.gen_a.encode(x_a)
+        h_b, n_b = self.gen_b.encode(x_b)
+        # decode (cross domain)
+        x_ba = self.gen_a.decode(h_b + n_b)
+        x_ab = self.gen_b.decode(h_a + n_a)
+        # D loss
+        self.loss_dis_a = self.dis_a.calc_dis_loss(x_ba.detach(), x_a)
+        self.loss_dis_b = self.dis_b.calc_dis_loss(x_ab.detach(), x_b)
+        self.loss_dis_total = hyperparameters['gan_w'] * self.loss_dis_a + hyperparameters['gan_w'] * self.loss_dis_b
+        self.loss_dis_total.backward()
+        self.dis_opt.step()
+
+    def update_learning_rate(self):
+        if self.dis_scheduler is not None:
+            self.dis_scheduler.step()
+        if self.gen_scheduler is not None:
