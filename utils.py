5fa91b3422f644456dbb48293e0b85c77728b0a5
Zim2erman2 <98412844+Zim2erman2@users.noreply.github.com>
2023-02-18 10:53:34 +0000

    Update wrapper

--- utils.py
+++ utils.py
@@ -19,3 +19,53 @@ import yaml
 import numpy as np
 import torch.nn.init as init
 import time
+# Methods
+# get_all_data_loaders      : primary data loader interface (load trainA, testA, trainB, testB)
+# get_data_loader_list      : list-based data loader
+# get_data_loader_folder    : folder-based data loader
+# get_config                : load yaml file
+# eformat                   :
+# write_2images             : save output image
+# prepare_sub_folder        : create checkpoints and images folders for saving outputs
+# write_one_row_html        : write one row of the html file for output images
+# write_html                : create the html file.
+# write_loss
+# slerp
+# get_slerp_interp
+# get_model_list
+# load_vgg16
+# load_inception
+# vgg_preprocess
+# get_scheduler
+# weights_init
+
+def get_all_data_loaders(conf):
+    batch_size = conf['batch_size']
+    num_workers = conf['num_workers']
+    if 'new_size' in conf:
+        new_size_a = new_size_b = conf['new_size']
+    else:
+        new_size_a = conf['new_size_a']
+        new_size_b = conf['new_size_b']
+    height = conf['crop_image_height']
+    width = conf['crop_image_width']
+
+    if 'data_root' in conf:
+        train_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'trainA'), batch_size, True,
+                                              new_size_a, height, width, num_workers, True)
+        test_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'testA'), batch_size, False,
+                                             new_size_a, new_size_a, new_size_a, num_workers, True)
+        train_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'trainB'), batch_size, True,
+                                              new_size_b, height, width, num_workers, True)
+        test_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'testB'), batch_size, False,
+                                             new_size_b, new_size_b, new_size_b, num_workers, True)
+    else:
+        train_loader_a = get_data_loader_list(conf['data_folder_train_a'], conf['data_list_train_a'], batch_size, True,
+                                                new_size_a, height, width, num_workers, True)
+        test_loader_a = get_data_loader_list(conf['data_folder_test_a'], conf['data_list_test_a'], batch_size, False,
+                                                new_size_a, new_size_a, new_size_a, num_workers, True)
+        train_loader_b = get_data_loader_list(conf['data_folder_train_b'], conf['data_list_train_b'], batch_size, True,
+                                                new_size_b, height, width, num_workers, True)
+        test_loader_b = get_data_loader_list(conf['data_folder_test_b'], conf['data_list_test_b'], batch_size, False,
+                                                new_size_b, new_size_b, new_size_b, num_workers, True)
+    return train_loader_a, train_loader_b, test_loader_a, test_loader_b
